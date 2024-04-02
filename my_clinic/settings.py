6d0b81ee49c9eb884ba473f79d4924e65da76c4b
Anas Dev <syriacano@gmail.com>
2024-04-02 17:17:52 +0200

    :add secrect key

--- my_clinic/settings.py
+++ my_clinic/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-q=x(y-ipg24p#dl07d)ewgsbzs3&u^7wze07kiu_1ygp*4p_12'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,6 +92,7 @@ WSGI_APPLICATION = 'my_clinic.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
