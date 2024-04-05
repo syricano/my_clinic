9798fd4bca783d969b632bf994b2187e2d36f9c0
Anas Dev <syriacano@gmail.com>
2024-04-05 07:04:50 +0200

    :fix admin pannel issue

--- my_clinic/settings.py
+++ my_clinic/settings.py
@@ -62,12 +62,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'booking.urls'
+ROOT_URLCONF = 'my_clinic.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
