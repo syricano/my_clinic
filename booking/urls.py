2d8e366a4ac75b092c0076f1679142376cc25705
Anas Dev <syriacano@gmail.com>
2024-04-06 01:31:21 +0200

    :rename the name in booking and home attributes

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [    
-    path('', views.makeAppointment.as_view(), name='booking'),
+    path('', views.makeAppointment.as_view(), name='appointments'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='index'),    
+    path('', views.HomePageView.as_view(), name='home'),    
 ]
