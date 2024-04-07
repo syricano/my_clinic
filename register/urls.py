1952ffd020101275f51b637148350f528a46b608
Anas Dev <syriacano@gmail.com>
2024-04-07 03:39:44 +0200

    :add html intial structure to base.html

--- register/urls.py
+++ register/urls.py
@@ -1,8 +1,8 @@
 from . import views
 from django.urls import path
 
-# URL patterns list for the 'contact' app
+# URL patterns list for the 'register' app
 urlpatterns = [
-    # Define a URL pattern for the ContactUs view
+    # Define a URL pattern for the RegisterUser view
     path('', views.RegisterUser.as_view(), name='register'),    
 ]
