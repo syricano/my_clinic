b3ebeee5688e7e9a63220140cbd8743a4debfb43
Anas Dev <syriacano@gmail.com>
2024-04-08 18:32:35 +0200

    :fix deployment bug

--- my_project/settings.py
+++ my_project/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
+
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static",]
 else:
-    STATIC_ROOT = BASE_DIR / "static"
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
